4be5f2fb18686e13bf70db850b5c839d4317ba6b
Luz Veninga <luzveninga@gmail.com>
2023-10-05 11:04:43 -0600

    se evita el uso de while true agregando la variable continuar

--- main.js
+++ main.js
@@ -1,54 +1,51 @@
+function calcularPrecioProducto() {
+    let continuar = true;
+
 //Se solicita el precio bruto
-let precio = Number(prompt("Ingrese el precio de costo del producto en $"));
-while(true){
-    if (!isNaN(precio) && precio!="" && precio !=null){
-        break;
-    }
-    else{
-        alert('No es número, ingrese el precio por favor');
-        precio = Number(prompt("Ingrese el precio de costo del producto en $"));
-    } 
 
-}
-alert(`El precio de costo del producto a calcular es: $${precio}`)  
+while (continuar) {
+    let precio = Number(prompt("Ingrese el precio de costo del producto en $"));
 
+    if (!isNaN(precio) && precio !== "" && precio !== null) {
+        alert(`El precio de costo del producto a calcular es: $${precio}`);
+        function calcularIva(precio) {
+          return (precio * 0.21);
+        }
 //Se calcula el iva del producto
 
-function calcularIva (precio){
-    return (precio * 0.21)
-}
+    let iva = calcularIva(precio);
+    iva =iva.toFixed(2)
+    alert(`El iva del producto seleccionado es: $${iva}`); 
 
-let iva= calcularIva(precio);
-alert (`El iva del producto seleccionado es: $${iva}`)
+    function sumarIva(precio) {
+    return (precio + (precio * 0.21));
+    }
 
 //sumamos el iva y obtenemos el precio neto
+    let precioConIva = sumarIva(precio);
+    precioConIva = precioConIva.toFixed(2);
+    alert(`El precio del producto con iva incluido es: $${precioConIva}`);
 
-function sumarIva(precio){
-    return(precio + (precio*0.21));
+//se sugiere precio de venta
+    function calcularPrecioVenta(precioConIva) {
+      return (precioConIva * 1.25);
+    }
+    let precioVenta = calcularPrecioVenta(precioConIva);
+    precioVenta = precioVenta.toFixed(2);
+    alert(`El precio de venta sugerido es: $${precioVenta}`);
+
+    let respuesta = prompt('¿Desea ingresar el costo de otro producto? Ingrese SI para continuar o NO para terminar').toLowerCase();
+    if (respuesta === 'si') {
+        continuar = true;  // Continuar con el proceso
+        } else {
+        continuar = false;  // Detener el proceso
+        alert('Un gusto servirlo, esperamos verlo pronto');
+        }
+    } else {
+        alert('No es un número válido. Ingrese el precio por favor');
+        }
+    }
 }
-
-let precioConIva = sumarIva(precio);
-alert(`El precio del producto con iva incluido es: $${precioConIva}`)
-
-//precio sugerido de venta
-
-function calcularPrecioVenta(precioConIva){
-    return(precioConIva * 1.25);
-}
-let precioVenta = calcularPrecioVenta(precioConIva);
-precioVenta= precioVenta.toFixed(2);
-
-alert(`El precio de venta sugerido es: $${precioVenta}`);
-alert('¿Desea ingresar el costo de otro producto?')
-let respuesta= prompt('Ingrese SI para continuar o NO para terminar').toLowerCase ();
-// while (true){
-//     if (respuesta === 'si'){
-//     precio = Number(prompt("Ingrese el precio de costo del producto en $"));
-// }
-//     else (respuesta==='no'){
-//     alert('Un gusto servirlo, esperamos verlo pronto');
-// }
-// }
-
+calcularPrecioProducto();  // Llama a la función por primera vez
 
 
